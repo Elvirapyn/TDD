845c9302cad45320e4fffb63d629dd649da0f8af
Elvirapyn <675063826@qq.com>
2020-05-27 20:03:57 +0800

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -4,14 +4,20 @@ from django.http import HttpRequest
 from lists.views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
-    def test_root_url_resolve_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func,home_page)
-        
+    # def test_root_url_resolve_to_home_page_view(self):
+    #     found = resolve('/')
+    #     self.assertEqual(found.func,home_page)
+    #
     def test_home_page_returns_coorect_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists </title>',html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        #
+        # html = response.content.decode('utf8')   #utf8解码后
+        # self.assertTrue(html.startswith('<html>'))
+        # self.assertIn('<title>To-Do lists</title>',html)
+        # self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response,'home.html')
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
